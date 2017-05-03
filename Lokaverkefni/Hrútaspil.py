a4587b1cf77662ba80a6881fb2d84184f4e1d021
Hrannsi <hrannareythors@gmail.com>
2017-05-03 15:01:00 +0000

    Add files via upload

--- Lokaverkefni/Hrútaspil.py
+++ Lokaverkefni/Hrútaspil.py
@@ -1,1 +1,53 @@
 #Hrannar Örn Eyþórsson
+listi = []
+with open("Hrutar.txt") as f:
+    for line in f:
+        if line:
+            listi.append(line)
+print(listi[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+jafntefli
+jafnt = []
+jafnt.append(stokkur)
+jafnt.append(stokkur2)
+jafnt = []
+'''
