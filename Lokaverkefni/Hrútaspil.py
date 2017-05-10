77c9b4848bf1fcb14fcc18a5688edf3a67929778
Hrannsi <hrannareythors@gmail.com>
2017-05-10 13:41:28 +0000

    Lét kóðan loksins virka
    Það er hægt að vinna og tapa, en tölvan getur enn ekki gert og ekki er hægt að lenda í jafntefli

--- Lokaverkefni/Hrútaspil.py
+++ Lokaverkefni/Hrútaspil.py
@@ -18,37 +18,13 @@ while len(stokkur2) != len(listi)/2:
         stokkur2.append(listi[val2])
         aftur.append(val2)
 temp = []
-print(stokkur[1][1:])
-'''
-stokkur = list(map(float, stokkur[0][1:]))
-stokkur = list(map(float, stokkur[1][1:]))
-stokkur = list(map(float, stokkur[2][1:]))
-stokkur = list(map(float, stokkur[3][1:]))
-stokkur = list(map(float, stokkur[4][1:]))
-stokkur = list(map(float, stokkur[5][1:]))
-stokkur = list(map(float, stokkur[6][1:]))
-stokkur = list(map(float, stokkur[7][1:]))
-stokkur = list(map(float, stokkur[8][1:]))
-stokkur = list(map(float, stokkur[9][1:]))
-stokkur2 = list(map(float, stokkur2[0][1:]))
-stokkur2 = list(map(float, stokkur2[1][1:]))
-stokkur2 = list(map(float, stokkur2[2][1:]))
-stokkur2 = list(map(float, stokkur2[3][1:]))
-stokkur2 = list(map(float, stokkur2[4][1:]))
-stokkur2 = list(map(float, stokkur2[5][1:]))
-stokkur2 = list(map(float, stokkur2[6][1:]))
-stokkur2 = list(map(float, stokkur2[7][1:]))
-stokkur2 = list(map(float, stokkur2[8][1:]))
-stokkur2 = list(map(float, stokkur2[9][1:]))
-'''
-print(stokkur)
-print(stokkur2)
-stokkur = list(map(float, stokkur[0:][1:]))
 
 print("Þegar þú ert að velja á eftir þá notar þú tölu stafi ekki orð, þar sem 1 = þyngd, 2 = Mjólk, 3 = ull, 4 = afkvæmi, 5 = lærvöðvi, 6 = frjósemi, 7 = kjöt og 8 = rassvöðvi")
 
 while (len(stokkur) != 0) or (len(stokkur2) != 0):
-    print(stokkur[0])
+    print('spilið þitt er:')
+    print('Nafn:', stokkur[0][0])
+    print('1. Þyngd:', stokkur[0][1])
     val = input("Hvað ætlar þú að nota: ")
     if val == "1":
         if stokkur[0][1] > stokkur2[0][1]:
@@ -58,10 +34,7 @@ while (len(stokkur) != 0) or (len(stokkur2) != 0):
             stokkur.remove(stokkur[0])
             stokkur.append(temp[0])
             temp.remove(temp[0])
-        else:
-            print("ERROR")
-    elif val == "1":
-        if stokkur[0][1] < stokkur2[0][1]:
+        elif stokkur[0][1] < stokkur2[0][1]:
             stokkur2.append(stokkur[0])
             stokkur.remove([0])
             temp.append(stokkur2[0])
