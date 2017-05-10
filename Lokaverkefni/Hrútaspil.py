5c4c94e8c681f182bfebbf30a6cbfb40e48b4745
Hrannsi <hrannareythors@gmail.com>
2017-05-10 13:53:15 +0000

    Núna getur tölvan gert

--- Lokaverkefni/Hrútaspil.py
+++ Lokaverkefni/Hrútaspil.py
@@ -18,35 +18,80 @@ while len(stokkur2) != len(listi)/2:
         stokkur2.append(listi[val2])
         aftur.append(val2)
 temp = []
-
-print("Þegar þú ert að velja á eftir þá notar þú tölu stafi ekki orð, þar sem 1 = þyngd, 2 = Mjólk, 3 = ull, 4 = afkvæmi, 5 = lærvöðvi, 6 = frjósemi, 7 = kjöt og 8 = rassvöðvi")
-
+tala = 0
 while (len(stokkur) != 0) or (len(stokkur2) != 0):
-    print('spilið þitt er:')
-    print('Nafn:', stokkur[0][0])
-    print('1. Þyngd:', stokkur[0][1])
-    val = input("Hvað ætlar þú að nota: ")
-    if val == "1":
-        if stokkur[0][1] > stokkur2[0][1]:
+    if tala % 2 == 0:
+        print('spilið þitt er:')
+        print('Nafn:', stokkur[0][0])
+        print('1. Þyngd:', stokkur[0][1])
+        print('2. Þyngd:', stokkur[0][2])
+        print('3. Þyngd:', stokkur[0][3])
+        print('4. Þyngd:', stokkur[0][4])
+        print('5. Þyngd:', stokkur[0][5])
+        print('6. Þyngd:', stokkur[0][6])
+        print('7. Þyngd:', stokkur[0][7])
+        print('8. Þyngd:', stokkur[0][8])
+        val = int(input("Hvað ætlar þú að nota: "))
+        if stokkur[0][val] > stokkur2[0][val]:
+            print("Tölvan var með:",stokkur2[0][val])
             stokkur.append(stokkur2[0])
             stokkur2.remove(stokkur2[0])
             temp.append(stokkur[0])
             stokkur.remove(stokkur[0])
             stokkur.append(temp[0])
             temp.remove(temp[0])
-        elif stokkur[0][1] < stokkur2[0][1]:
+            print("Sigur")
+            print()
+            tala += 1
+        elif stokkur[0][val] < stokkur2[0][val]:
+            print("Tölvan var með:", stokkur2[0][val])
             stokkur2.append(stokkur[0])
-            stokkur.remove([0])
+            stokkur.remove(stokkur[0])
             temp.append(stokkur2[0])
-            stokkur2.remove([0])
+            stokkur2.remove(stokkur2[0])
             stokkur2.append(temp[0])
-            temp.remove([0])
+            temp.remove(temp[0])
+            print("Tap")
+            print()
+            tala += 1
         else:
             print("ERROR")
-    else:
-        print("ERROR")
-    print(stokkur)
-    print(stokkur2)
+    elif tala % 2 == 1:
+        print('spil tölvunar er:')
+        print('Nafn:', stokkur2[0][0])
+        print('1. Þyngd:', stokkur2[0][1])
+        print('2. Þyngd:', stokkur2[0][2])
+        print('3. Þyngd:', stokkur2[0][3])
+        print('4. Þyngd:', stokkur2[0][4])
+        print('5. Þyngd:', stokkur2[0][5])
+        print('6. Þyngd:', stokkur2[0][6])
+        print('7. Þyngd:', stokkur2[0][7])
+        print('8. Þyngd:', stokkur2[0][8])
+        val = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
+        if stokkur[0][val] > stokkur2[0][val]:
+            print("Þú varst með:", stokkur[0][val])
+            stokkur.append(stokkur2[0])
+            stokkur2.remove(stokkur2[0])
+            temp.append(stokkur[0])
+            stokkur.remove(stokkur[0])
+            stokkur.append(temp[0])
+            temp.remove(temp[0])
+            print("Sigur")
+            print()
+            tala += 1
+        elif stokkur[0][val] < stokkur2[0][val]:
+            print("Þú varst með:", stokkur[0][val])
+            stokkur2.append(stokkur[0])
+            stokkur.remove(stokkur[0])
+            temp.append(stokkur2[0])
+            stokkur2.remove(stokkur2[0])
+            stokkur2.append(temp[0])
+            temp.remove(temp[0])
+            print("Tap")
+            print()
+            tala += 1
+        else:
+            print("ERROR")
 
 
 
